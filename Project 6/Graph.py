2fa8350437c4061f3e116907e4ae49e4edb8ace9
chandnas4 <70782198+chandnas4@users.noreply.github.com>
2022-06-28 23:00:57 +0530

    Add files via upload

---
+++ Project 6/Graph.py
@@ -0,0 +1,433 @@
+"""
+Name: Shubham Chandna
+CSE 331 FS20 (Onsay)
+"""
+
+import heapq
+import itertools
+import math
+import queue
+import random
+import time
+from typing import TypeVar, Callable, Tuple, List, Set
+
+import matplotlib.cm as cm
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
+
+T = TypeVar('T')
+Matrix = TypeVar('Matrix')  # Adjacency Matrix
+Vertex = TypeVar('Vertex')  # Vertex Class Instance
+Graph = TypeVar('Graph')    # Graph Class Instance
+
+
+class Vertex:
+    """ Class representing a Vertex object within a Graph """
+
+    __slots__ = ['id', 'adj', 'visited', 'x', 'y']
+
+    def __init__(self, idx: str, x: float = 0, y: float = 0) -> None:
+        """
+        DO NOT MODIFY
+        Initializes a Vertex
+        :param idx: A unique string identifier used for hashing the vertex
+        :param x: The x coordinate of this vertex (used in a_star)
+        :param y: The y coordinate of this vertex (used in a_star)
+        """
+        self.id = idx
+        self.adj = {}             # dictionary {id : weight} of outgoing edges
+        self.visited = False      # boolean flag used in search algorithms
+        self.x, self.y = x, y     # coordinates for use in metric computations
+
+    def __eq__(self, other: Vertex) -> bool:
+        """
+        DO NOT MODIFY
+        Equality operator for Graph Vertex class
+        :param other: vertex to compare
+        """
+        if self.id != other.id:
+            return False
+        elif self.visited != other.visited:
+            print(f"Vertex '{self.id}' not equal")
+            print(f"Vertex visited flags not equal: self.visited={self.visited},"
+                  f" other.visited={other.visited}")
+            return False
+        elif self.x != other.x:
+            print(f"Vertex '{self.id}' not equal")
+            print(f"Vertex x coords not equal: self.x={self.x}, other.x={other.x}")
+            return False
+        elif self.y != other.y:
+            print(f"Vertex '{self.id}' not equal")
+            print(f"Vertex y coords not equal: self.y={self.y}, other.y={other.y}")
+            return False
+        elif set(self.adj.items()) != set(other.adj.items()):
+            diff = set(self.adj.items()).symmetric_difference(set(other.adj.items()))
+            print(f"Vertex '{self.id}' not equal")
+            print(f"Vertex adj dictionaries not equal:"
+                  f" symmetric diff of adjacency (k,v) pairs = {str(diff)}")
+            return False
+        return True
+
+    def __repr__(self) -> str:
+        """
+        DO NOT MODIFY
+        Represents Vertex object as string.
+        :return: string representing Vertex object
+        """
+        lst = [f"<id: '{k}', weight: {v}>" for k, v in self.adj.items()]
+
+        return f"<id: '{self.id}'" + ", Adjacencies: " + "".join(lst) + ">"
+
+    def __str__(self) -> str:
+        """
+        DO NOT MODIFY
+        Represents Vertex object as string.
+        :return: string representing Vertex object
+        """
+        return repr(self)
+
+    def __hash__(self) -> int:
+        """
+        DO NOT MODIFY
+        Hashes Vertex into a set; used in unit tests
+        :return: hash value of Vertex
+        """
+        return hash(self.id)
+
+#============== Modify Vertex Methods Below ==============#
+
+    def degree(self) -> int:
+        """
+        Returns the number of outgoing edges from this vertex
+        :return: number of edges
+        """
+        return len(self.adj)
+
+    def get_edges(self) -> Set[Tuple[str, float]]:
+        """
+        Returns a set of tuples representing outgoing edges from vertex
+        :return: set of tuples
+        """
+        edges = set()
+        for key in self.adj:
+            edges.add((key, self.adj[key]))
+        return edges
+
+    def euclidean_distance(self, other: Vertex) -> float:
+        """
+        Returns the euclidean distance
+        :param other: the vertex to which distance is calculated
+        :return: distance in float
+        """
+
+        distance = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+        return distance
+
+    def taxicab_distance(self, other: Vertex) -> float:
+        """
+        Returns the taxicab distance
+        :param other: the vertex to which distance is calculated
+        :return: distnace in float
+        """
+        if self.x < 0:
+            self.x = -1 * self.x
+        if self.y < 0:
+            self.y = -1 * self.x
+        if other.x < 0:
+            other.x = -1 * other.x
+        if other.y < 0:
+            other.y = -1 * other.y
+
+        return int(self.x + self.y + other.x + other.y)
+
+
+class Graph:
+    """ Class implementing the Graph ADT using an Adjacency Map structure """
+
+    __slots__ = ['size', 'vertices', 'plot_show', 'plot_delay']
+
+    def __init__(self, plt_show: bool = False, matrix: Matrix = None, csv: str = "") -> None:
+        """
+        DO NOT MODIFY
+        Instantiates a Graph class instance
+        :param: plt_show : if true, render plot when plot() is called; else, ignore calls to plot()
+        :param: matrix : optional matrix parameter used for fast construction
+        :param: csv : optional filepath to a csv containing a matrix
+        """
+        matrix = matrix if matrix else np.loadtxt(csv, delimiter=',', dtype=str).tolist() if csv else None
+        self.size = 0
+        self.vertices = {}
+
+        self.plot_show = plt_show
+        self.plot_delay = 0.2
+
+        if matrix is not None:
+            for i in range(1, len(matrix)):
+                for j in range(1, len(matrix)):
+                    if matrix[i][j] == "None" or matrix[i][j] == "":
+                        matrix[i][j] = None
+                    else:
+                        matrix[i][j] = float(matrix[i][j])
+            self.matrix2graph(matrix)
+
+    def __eq__(self, other: Graph) -> bool:
+        """
+        DO NOT MODIFY
+        Overloads equality operator for Graph class
+        :param other: graph to compare
+        """
+        if self.size != other.size or len(self.vertices) != len(other.vertices):
+            print(f"Graph size not equal: self.size={self.size}, other.size={other.size}")
+            return False
+        for vertex_id, vertex in self.vertices.items():
+            other_vertex = other.get_vertex(vertex_id)
+            if other_vertex is None:
+                print(f"Vertices not equal: '{vertex_id}' not in other graph")
+                return False
+
+            adj_set = set(vertex.adj.items())
+            other_adj_set = set(other_vertex.adj.items())
+
+            if not adj_set == other_adj_set:
+                print(f"Vertices not equal: adjacencies of '{vertex_id}' not equal")
+                print(f"Adjacency symmetric difference = "
+                      f"{str(adj_set.symmetric_difference(other_adj_set))}")
+                return False
+        return True
+
+    def __repr__(self) -> str:
+        """
+        DO NOT MODIFY
+        Represents Graph object as string.
+        :return: String representation of graph for debugging
+        """
+        return "Size: " + str(self.size) + ", Vertices: " + str(list(self.vertices.items()))
+
+    def __str__(self) -> str:
+        """
+        DO NOT MODIFY
+        Represents Graph object as string.
+        :return: String representation of graph for debugging
+        """
+        return repr(self)
+
+    def plot(self) -> None:
+        """
+        DO NOT MODIFY
+        Creates a plot a visual representation of the graph using matplotlib
+        :return: None
+        """
+        if self.plot_show:
+
+            # if no x, y coords are specified, place vertices on the unit circle
+            for i, vertex in enumerate(self.get_vertices()):
+                if vertex.x == 0 and vertex.y == 0:
+                    vertex.x = math.cos(i * 2 * math.pi / self.size)
+                    vertex.y = math.sin(i * 2 * math.pi / self.size)
+
+            # show edges
+            num_edges = len(self.get_edges())
+            max_weight = max([edge[2] for edge in self.get_edges()]) if num_edges > 0 else 0
+            colormap = cm.get_cmap('cool')
+            for i, edge in enumerate(self.get_edges()):
+                origin = self.get_vertex(edge[0])
+                destination = self.get_vertex(edge[1])
+                weight = edge[2]
+
+                # plot edge
+                arrow = patches.FancyArrowPatch((origin.x, origin.y),
+                                                (destination.x, destination.y),
+                                                connectionstyle="arc3,rad=.2",
+                                                color=colormap(weight / max_weight),
+                                                zorder=0,
+                                                **dict(arrowstyle="Simple,tail_width=0.5,"
+                                                                  "head_width=8,head_length=8"))
+                plt.gca().add_patch(arrow)
+
+                # label edge
+                plt.text(x=(origin.x + destination.x) / 2 - (origin.x - destination.x) / 10,
+                         y=(origin.y + destination.y) / 2 - (origin.y - destination.y) / 10,
+                         s=weight, color=colormap(weight / max_weight))
+
+            # show vertices
+            x = np.array([vertex.x for vertex in self.get_vertices()])
+            y = np.array([vertex.y for vertex in self.get_vertices()])
+            labels = np.array([vertex.id for vertex in self.get_vertices()])
+            colors = np.array(
+                ['yellow' if vertex.visited else 'black' for vertex in self.get_vertices()])
+            plt.scatter(x, y, s=40, c=colors, zorder=1)
+
+            # plot labels
+            for j, _ in enumerate(x):
+                plt.text(x[j] - 0.03*max(x), y[j] - 0.03*max(y), labels[j])
+
+            # show plot
+            plt.show()
+            # delay execution to enable animation
+            time.sleep(self.plot_delay)
+
+#============== Modify Graph Methods Below ==============#
+
+    def reset_vertices(self) -> None:
+        """
+        Resets visited flags of all vertices within the graph
+        :return: None
+        """
+        for vertice in self.vertices.values():
+            vertice.visited = False
+
+    def get_vertex(self, vertex_id: str) -> Vertex:
+        """
+        Returns the Vertex object with id vertex_id if it exists in the graph
+        :param vertex_id:
+        :return: vertex
+        """
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        return None
+
+    def get_vertices(self) -> Set[Vertex]:
+        """
+        Returns a set of all Vertex objects held in the graph
+        :return: set of vertices
+        """
+        set_ver = set()
+        for value in self.vertices.values():
+            set_ver.add(value)
+        return set_ver
+
+    def get_edge(self, start_id: str, dest_id: str) -> Tuple[str, str, float]:
+        """
+        Returns the edge connecting the two vertices
+        :param start_id: start vertex
+        :param dest_id: end vertex
+        :return: the edge
+        """
+        if start_id in self.vertices and dest_id in self.vertices[start_id].adj:
+            return start_id, dest_id, self.vertices[start_id].adj[dest_id]
+        return None
+
+    def get_edges(self) -> Set[Tuple[str, str, float]]:
+        """
+        Returns a set of tuples representing all edges within the graph
+        :return: a set of tuple of all edges
+        """
+        setedges = set()
+        for start_id in self.vertices:
+            for dest_id in self.vertices[start_id].adj:
+                setedges.add((start_id, dest_id, self.vertices[start_id].adj[dest_id]))
+        return setedges
+
+    def add_to_graph(self, start_id: str, dest_id: str = None, weight: float = 0) -> None:
+        """
+        Adds a vertex /vertices / edge to the graph
+        :param start_id: start vertex
+        :param dest_id: end vertex
+        :param weight: the dge
+        :return: None
+        """
+
+        if start_id not in self.vertices:
+            self.vertices[start_id] = Vertex(start_id)
+            self.size += 1
+
+        if dest_id is not None:
+            if dest_id not in self.vertices:
+                self.vertices[dest_id] = Vertex(dest_id)
+                self.size += 1
+            if weight != 0:
+                self.vertices[start_id].adj[dest_id] = weight
+
+    def matrix2graph(self, matrix: Matrix) -> None:
+        """
+        Constructs a graph from a given adjacency matrix representation
+        :param matrix:
+        :return: None
+        """
+        if matrix != [[]]:
+            for i in range(1, len(matrix)):
+                for j in range(1, len(matrix)):
+                    if matrix[i][j] is not None:
+                        self.add_to_graph(matrix[i][0], matrix[0][j], matrix[i][j])
+                    self.add_to_graph(matrix[i][0], matrix[0][j])
+
+
+class AStarPriorityQueue:
+    """
+    Priority Queue built upon heapq module with support for priority key updates
+    Created by Andrew McDonald
+    Inspired by https://docs.python.org/3/library/heapq.html
+    """
+
+    __slots__ = ['data', 'locator', 'counter']
+
+    def __init__(self) -> None:
+        """
+        Construct an AStarPriorityQueue object
+        """
+        self.data = []                        # underlying data list of priority queue
+        self.locator = {}                     # dictionary to locate vertices within priority queue
+        self.counter = itertools.count()      # used to break ties in prioritization
+
+    def __repr__(self) -> str:
+        """
+        Represent AStarPriorityQueue as a string
+        :return: string representation of AStarPriorityQueue object
+        """
+        lst = [f"[{priority}, {vertex}], " if vertex is not None else "" for
+               priority, count, vertex in self.data]
+        return "".join(lst)[:-1]
+
+    def __str__(self) -> str:
+        """
+        Represent AStarPriorityQueue as a string
+        :return: string representation of AStarPriorityQueue object
+        """
+        return repr(self)
+
+    def empty(self) -> bool:
+        """
+        Determine whether priority queue is empty
+        :return: True if queue is empty, else false
+        """
+        return len(self.data) == 0
+
+    def push(self, priority: float, vertex: Vertex) -> None:
+        """
+        Push a vertex onto the priority queue with a given priority
+        :param priority: priority key upon which to order vertex
+        :param vertex: Vertex object to be stored in the priority queue
+        :return: None
+        """
+        # list is stored by reference, so updating will update all refs
+        node = [priority, next(self.counter), vertex]
+        self.locator[vertex.id] = node
+        heapq.heappush(self.data, node)
+
+    def pop(self) -> Tuple[float, Vertex]:
+        """
+        Remove and return the (priority, vertex) tuple with lowest priority key
+        :return: (priority, vertex) tuple where priority is key,
+        and vertex is Vertex object stored in priority queue
+        """
+        vertex = None
+        while vertex is None:
+            # keep popping until we have valid entry
+            priority, count, vertex = heapq.heappop(self.data)
+        del self.locator[vertex.id]            # remove from locator dict
+        vertex.visited = True                  # indicate that this vertex was visited
+        while len(self.data) > 0 and self.data[0][2] is None:
+            heapq.heappop(self.data)          # delete trailing Nones
+        return priority, vertex
+
+    def update(self, new_priority: float, vertex: Vertex) -> None:
+        """
+        Update given Vertex object in the priority queue to have new priority
+        :param new_priority: new priority on which to order vertex
+        :param vertex: Vertex object for which priority is to be updated
+        :return: None
+        """
+        node = self.locator.pop(vertex.id)      # delete from dictionary
+        node[-1] = None                         # invalidate old node
+        self.push(new_priority, vertex)         # push new node
--- Project 6/README.md
+++ Project 6/README.md
@@ -1,1 +1,11 @@
+# Name
+Shubham Chandna
 
+# Feedback
+Difficulty level was similiar to the projects before
+
+# Time to Completion
+2 hours
+
+# Citations
+Professor Onsay's starter code
